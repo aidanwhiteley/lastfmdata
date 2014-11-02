dc9542df6bc6e9f99b817eadebb4bff907e5548e
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2014-11-02 12:05:50 +0000

    Improved use of ngTable - efficiency
    Avoid call to http data service for each pagination request. Code
    restructured to hold ngTable directly on scope and call reload on it as
    necessary

--- app/scripts/controllers/toptracks.js
+++ app/scripts/controllers/toptracks.js
@@ -12,8 +12,9 @@ angular.module('lastfmdataApp')
         function($scope, appConstants, lastFmDataService, ngTableParams, controllerUtils) {
 
             $scope.timePeriod = '3month';
-            $scope.total = 50;
 			$scope.displayPeriod = ' over the last 3 months';
+			$scope.data = [];
+            $scope.initialPageLoad = true;
 
             if (appConstants.lastFmUser.indexOf('xxx') >= 0 || appConstants.lastFmApiKey.indexOf('000') >= 0) {
                 $scope.editConstantsError = true;
@@ -23,42 +24,45 @@ angular.module('lastfmdataApp')
             $scope.setPeriod = function(selectedDuration) {
                 $scope.timePeriod = selectedDuration;
 				$scope.displayPeriod = controllerUtils.getDisplayTimePeriod(selectedDuration);
-                $scope.tableParams.reload();
+				$scope.getTopTracks();
             };
-
-            /*
-                Having the getData function call the lastFmDataService.getTopTracks service only
-                works from an efficiency point of view because the service caches the responses from
-                http calls. If it was making a call to last.fm every time (e.g. every page forward in the table)
-                this would be very efficient).
-                TO-DO - sort out that the $scope.total is basically hard coded to 50. This will break!
-            */
+            
             $scope.tableParams = new ngTableParams({
                 page: 1,
                 count: 10
             }, {
-                total: $scope.total,
+                total: $scope.data.length,
                 getData: function($defer, params) {
-                    $scope.isLoading = true;
-                    lastFmDataService.getTopTracks($scope.timePeriod)
-                        .then(
-                            function(data) {
-                                var hasData = (data.toptracks.track != null);
-                                if (hasData) {
-                                    $scope.total = data.toptracks.track.length;
-                                }
-                                var page = hasData ? data.toptracks.track.slice((params.page() - 1) * params.count(), params.page() * params.count()) : [];
-                                $defer.resolve(page);
-                                $scope.isLoading = false;
-
-                            },
-                            function() {
-                                $scope.dataRetrievalError = true;
-                                $scope.isLoading = false;
-                            }
-                        );
-
+                    var hasData = ($scope.data && $scope.data.toptracks && $scope.data.toptracks.track != null);
+                    if (hasData) {
+                        params.total($scope.data.toptracks.track.length);
+                    }
+                    var page = hasData ? $scope.data.toptracks.track.slice((params.page() - 1) * params.count(), params.page() * params.count()) : [];
+                    $defer.resolve(page);
+                    $scope.isLoading = false;
                 }
             });
+		
+			$scope.getTopTracks = function() {
+                $scope.isLoading = true;
+				$scope.initialPageLoad = false;
+                
+				lastFmDataService.getTopTracks($scope.timePeriod)
+                    .then(
+                        function(data) {
+                            $scope.data = data;
+                            $scope.tableParams.reload();
+                            $scope.tableParams.$params.page = 1;
+                        },
+                        function() {
+                            $scope.dataRetrievalError = true;
+                            $scope.isLoading = false;
+                        }
+                    );
+			};
+			
+			if ($scope.initialPageLoad) {
+				$scope.getTopTracks();
+			}
         }
     ]);
